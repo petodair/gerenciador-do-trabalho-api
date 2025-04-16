0eeeb4bcacb4b5febf18381b02c4beb158f9df25
Peterson Odair <137333120+petodair@users.noreply.github.com>
2025-04-15 23:06:22 -0300

    commit inicial

---
+++ src/main/java/br/com/gerenciador_do_trabalho_api/GerenciadorDoTrabalhoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciador_do_trabalho_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDoTrabalhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDoTrabalhoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerenciador_do_trabalho_api/GerenciadorDoTrabalhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciador_do_trabalho_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDoTrabalhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
